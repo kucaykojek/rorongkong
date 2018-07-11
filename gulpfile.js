ff58a0e8482637885ff74310342b16e9224c90c9
Newbielabs Studio <newbielabs@gmail.com>
2018-07-11 11:46:24 +0700

    Adding js and fix error

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,73 @@
-const browserSync = require('browser-sync');
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const gulpif = require('gulp-if');
-const sourcemaps = require('gulp-sourcemaps');
-const autoprefixer = require('gulp-autoprefixer');
-const gutil = require('gulp-util');
-const del = require('del');
+const browserSync = require("browser-sync");
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const gulpif = require("gulp-if");
+const sourcemaps = require("gulp-sourcemaps");
+const autoprefixer = require("gulp-autoprefixer");
+const gutil = require("gulp-util");
+const babel = require("gulp-babel");
+const del = require("del");
 
 const env = process.env.NODE_ENV;
 
-gulp.task('browserSync', function () {
+gulp.task("browserSync", function () {
   browserSync({
     server: {
-      baseDir: './public/',
+      baseDir: "./public/",
     },
   });
 });
 
-gulp.task('html', function () {
-  return gulp.src('public/*.html')
+gulp.task("html", function () {
+  return gulp.src("public/*.html")
     .pipe(browserSync.stream());
 });
 
-gulp.task('styles', function () {
-  gulp.src(['src/scss/*.scss', 'src/scss/**/*.scss'])
+gulp.task("scripts", function () {
+  return gulp
+    .src([
+      "node_modules/babel-polyfill/dist/polyfill.js",
+      "src/js/*.js",
+      "src/js/**/*.js"
+    ])
+    .pipe(babel({ presets: ["babel-preset-env"] }))
+    .pipe(gulp.dest("public/js"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("styles", function () {
+  gulp.src(["src/scss/*.scss", "src/scss/**/*.scss"])
     .pipe(sourcemaps.init())
 
     // scss output compressed if production or expanded if development
-    .pipe(gulpif(env === 'production', sass({ outputStyle: 'compressed' }),
-      sass({ outputStyle: 'expanded' })))
-    .on('error', gutil.log.bind(gutil, gutil.colors.red(
-      '\n\n*********************************** \n' +
-      'SASS ERROR:' +
-      '\n*********************************** \n\n'
+    .pipe(gulpif(env === "production", sass({ outputStyle: "compressed" }),
+      sass({ outputStyle: "expanded" })))
+    .on("error", gutil.log.bind(gutil, gutil.colors.red(
+      "\n\n*********************************** \n" +
+      "SASS ERROR:" +
+      "\n*********************************** \n\n"
     )))
     .pipe(autoprefixer({
-      browsers: ['last 3 versions'],
+      browsers: ["last 3 versions"],
       cascade: false,
     }))
-    .pipe(gulpif(env === 'development', sourcemaps.write('../maps')))
-    .pipe(gulp.dest('public/css'))
+    .pipe(gulpif(env === "development", sourcemaps.write("../maps")))
+    .pipe(gulp.dest("public/css"))
     .pipe(browserSync.stream());
 });
 
-gulp.task('clean:maps', (env === 'production', deleteMapsFolder));
+gulp.task("clean:maps", (env === "production", deleteMapsFolder));
 
 function deleteMapsFolder() {
   return del([
-    'public//maps/**',
+    "public//maps/**",
   ]);
 }
 
-gulp.task('watch', function () {
-  gulp.watch('public/*.html', ['html']);
-  gulp.watch(['src/scss/*.scss', 'src/scss/**/*.scss'], ['styles']);
+gulp.task("watch", function () {
+  gulp.watch("public/*.html", ["html"]);
+  gulp.watch(["src/scss/*.scss", "src/scss/**/*.scss"], ["styles"]);
+  gulp.watch(["src/js/*.js", "src/js/**/*.js"], ["scripts"]);
 });
 
-gulp.task('default', ['styles', 'browserSync', 'clean:maps', 'watch']);
+gulp.task("default", ["scripts", "styles", "browserSync", "clean:maps", "watch"]);
---
+++ public/js/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var foo = function foo() {
+  return console.log('JS Loaded');
+};
+
+foo();
