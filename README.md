600c5172d45085a3757e12534c860553da960e1b
Asep Fajar Nugraha <asep.fajar@bukalapak.com>
2018-08-20 18:46:48 +0700

    Optimize & Simplified

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 
 # Rorongkong
 
-Starting kit to create awesomeness.
+Starting kit to create awesomeness. Develop your web application easily.
+
+#### How to use
+Running development:
+```sh
+npm i && npm start
+```
+
+Build:
+```sh
+npm run build
+```
+
+#### Features
+- Browsersync - For automating reload page & sync on different devices
+- Gulp - For automating tasks
+- Support **ES6** for your Javascript
+- Support **Sass** for your Stylesheet
+
+#### License
+MIT
--- gulpfile.js
+++ gulpfile.js
@@ -1,86 +1,103 @@
-const browserSync = require("browser-sync");
-const gulp = require("gulp");
-const sass = require("gulp-sass");
-const gulpif = require("gulp-if");
-const sourcemaps = require("gulp-sourcemaps");
-const autoprefixer = require("gulp-autoprefixer");
-const gutil = require("gulp-util");
-const babel = require("gulp-babel");
-const del = require("del");
+const browserSync = require('browser-sync');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const gulpif = require('gulp-if');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const babel = require('gulp-babel');
+const del = require('del');
 
 const env = process.env.NODE_ENV;
 
-gulp.task("browserSync", function () {
+// BrowserSync
+gulp.task('browserSync', function () {
   browserSync({
     server: {
-      baseDir: "./public/",
+      baseDir: './public/',
     },
   });
 });
 
-gulp.task("html", function () {
-  return gulp
-    .src(["src/html/*.html", "src/html/**/*.html"])
-    .pipe(gulp.dest("public"))
-    .pipe(browserSync.stream());
-});
-
-gulp.task("scripts", function () {
+// HTML
+gulp.task('html', function () {
   return gulp
     .src([
-      "node_modules/babel-polyfill/dist/polyfill.js",
-      "src/js/*.js",
-      "src/js/**/*.js"
+      'src/html/*.html',
+      'src/html/**/*.html'
     ])
-    .pipe(babel({ presets: ["babel-preset-env"] }))
-    .pipe(gulp.dest("public/js"))
-    .pipe(browserSync.stream());
+    .pipe(gulp.dest('public'))
+    .pipe(gulpif(env === 'development', browserSync.stream(), false));
 });
 
-gulp.task("styles", function () {
-  gulp
-    .src(["src/scss/*.scss", "src/scss/**/*.scss"])
-    .pipe(sourcemaps.init())
-
-    // scss output compressed if production or expanded if development
-    .pipe(gulpif(env === "production", sass({
-          outputStyle: "compressed"
-        }), sass({ outputStyle: "expanded" })))
-    .on("error", gutil.log.bind(gutil, gutil.colors.red("\n\n*********************************** \n" + "SASS ERROR:" + "\n*********************************** \n\n")))
-    .pipe(autoprefixer({ browsers: ["last 3 versions"], cascade: false }))
-    .pipe(gulpif(env === "development", sourcemaps.write("../maps")))
-    .pipe(gulp.dest("public/css"))
-    .pipe(browserSync.stream());
-});
-
-gulp.task("image", function() {
+// Javascript
+gulp.task('scripts', function () {
   return gulp
-    .src("src/img/*")
-    .pipe(gulp.dest("public/img"))
-    .pipe(browserSync.stream());
+    .src([
+      'node_modules/babel-polyfill/dist/polyfill.js',
+      'src/js/*.js',
+      'src/js/**/*.js'
+    ])
+    .pipe(babel({ presets: ['babel-preset-env'] }))
+    .pipe(gulp.dest('public/js'))
+    .pipe(gulpif(env === 'development', browserSync.stream(), false));
 });
 
-gulp.task("clean:maps", (env === "production", deleteMapsFolder));
+// Stylesheet
+gulp.task('styles', function () {
+  return gulp
+    .src(['src/scss/*.scss', 'src/scss/**/*.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(gulpif(env === 'production', sass({
+        outputStyle: 'compressed'
+      }), sass({ outputStyle: 'expanded' })))
+    .pipe(autoprefixer({ browsers: ['last 3 versions'], cascade: false }))
+    .pipe(gulpif(env === 'development', sourcemaps.write('../maps')))
+    .pipe(gulp.dest('public/css'))
+    .pipe(gulpif(env === 'development', browserSync.stream(), false));
+});
+
+// Assets
+gulp.task('image', function() {
+  return gulp
+    .src('src/img/*')
+    .pipe(gulp.dest('public/img'))
+    .pipe(gulpif(env === 'development', browserSync.stream(), false));
+});
+
+// Clean
+gulp.task('clean:maps', (env === 'production', deleteMapsFolder));
 
 function deleteMapsFolder() {
   return del([
-    "public//maps/**",
+    'public//maps/**',
   ]);
 }
 
-gulp.task("watch", function () {
-  gulp.watch("src/img/*", ["image"]);
-  gulp.watch(["src/html/*.html", "src/html/**/*.html"], ["html"]);
-  gulp.watch(["src/scss/*.scss", "src/scss/**/*.scss"], ["styles"]);
-  gulp.watch(["src/js/*.js", "src/js/**/*.js"], ["scripts"]);
+// Watch
+gulp.task('watch', function () {
+  gulp.watch('src/img/*', ['image']);
+  gulp.watch(['src/html/*.html', 'src/html/**/*.html'], ['html']);
+  gulp.watch(['src/scss/*.scss', 'src/scss/**/*.scss'], ['styles']);
+  gulp.watch(['src/js/*.js', 'src/js/**/*.js'], ['scripts']);
 });
 
-gulp.task("default", [
-  "html",
-  "styles",
-  "scripts",
-  "image",
-  "browserSync",
-  "clean:maps",
-  "watch"
+// Default tasks
+gulp.task(
+    'default', [
+    'html',
+    'styles',
+    'scripts',
+    'image',
+    'clean:maps',
+    'watch',
+    'browserSync'
+])
+
+// Build tasks
+gulp.task('build', [
+  'html',
+  'styles',
+  'scripts',
+  'image',
+  'clean:maps'
 ]);
